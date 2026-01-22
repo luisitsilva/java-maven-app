25f8d9d6d78d9109de0f050262b0a8e8e9b39083
luisitsilva <luisitsilva@protonmail.com>
2026-01-22 19:49:20 +0000

    Updated info on start.js

--- start.js
+++ start.js
@@ -1,1 +1,6 @@
 console.log('JAVA-MAVEN-APP Started');
+console.log('Listening on port 9000000');
+console.log('This is now version 30');
+
+const os = require('os');
+console.log('Hostname:', os.hostname());
